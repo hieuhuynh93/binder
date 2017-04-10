038756820b60ae12f81c712fd3c14dcb0f163a38
Matt Watson <matt@makedo.net>
2017-04-10 17:34:40 +0100

    Trait update

--- php/class-helper.php
+++ php/class-helper.php
@@ -20,9 +20,9 @@ namespace mkdo\binder;
  *
  * Then within the Helper Class include the traits with the 'Use' declaration.
  */
-require_once __DIR__ . '/../traits/trait-get-icons.php';
 require_once __DIR__ . '/../traits/trait-create-guid.php';
 require_once __DIR__ . '/../traits/trait-format-bytes.php';
+require_once __DIR__ . '/../traits/trait-get-icons.php';
 require_once __DIR__ . '/../traits/trait-get-image-sizes.php';
 require_once __DIR__ . '/../traits/trait-page-slug-from-url.php';
 require_once __DIR__ . '/../traits/trait-render-view.php';
@@ -48,11 +48,9 @@ class Helper {
 	 *
 	 * `Helper::convert_hashtags_to_twitter_urls( $content );`
 	 */
-
-
-	use Helper_Get_Icons;
 	use Helper_Create_GUID;
 	use Helper_Format_Bytes;
+	use Helper_Get_Icons;
 	use Helper_Get_Image_Sizes;
 	use Helper_Page_Slug_From_URL;
 	use Helper_Render_View;
